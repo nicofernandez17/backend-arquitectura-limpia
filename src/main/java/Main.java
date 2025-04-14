6ac8945458e8650992f2eb2d7f8b0ec351ab6525
Nicolas Fernandez <nfernndez@frba.utn.edu.ar>
2025-04-14 12:33:24 -0300

    Cambios lector

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import domain.Hecho;
 import lectores.Lector;
 import lectores.LectorCSV;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -7,10 +13,14 @@ public class Main {
 
         Lector lector = new LectorCSV();
 
-        lector.leer("desastres_naturales_argentina.csv").forEach(x -> System.out.println(x.getTitulo()));
+        //lector.leer("desastres_naturales_argentina.csv").forEach(x -> System.out.println(x.getTitulo()));
 
-
-
+        Set<String> titulosVistos = new HashSet<>();
+        List<Hecho> hechos = lector.leer("desastres_naturales_argentina.csv");
+        List<Hecho> hechosSinDuplicados = hechos.stream()
+                .filter(hecho -> titulosVistos.add(hecho.getTitulo()))
+                .collect(Collectors.toList());
+        hechosSinDuplicados.forEach(h -> System.out.println(h.getTitulo()));
     }
 
 
