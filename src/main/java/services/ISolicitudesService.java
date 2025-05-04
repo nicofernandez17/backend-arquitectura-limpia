557396e70dc353ede0348997b596d115df1bc884
Nicolas <nicofsc@gmail.com>
2025-05-04 18:47:30 -0300

    Serices

--- src/main/java/services/ISolicitudesService.java
+++ src/main/java/services/ISolicitudesService.java
@@ -1,4 +1,8 @@
 package services;
 
+import models.dtos.input.SolicitudInputDTO;
+import models.dtos.output.SolicitudOutputDTO;
+
 public interface ISolicitudesService {
+    public SolicitudOutputDTO crearSolicitud(SolicitudInputDTO solicitudDTO);
 }
