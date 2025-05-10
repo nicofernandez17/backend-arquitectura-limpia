07f42cd3d59cbafc9789eacf7ab85de437c66fc3
Nicolas <nicofsc@gmail.com>
2025-05-10 19:45:56 -0300

    Manejo con coleccionDTO

--- components/agregador/src/main/java/utn/repositories/ColeccionRepository.java
+++ components/agregador/src/main/java/utn/repositories/ColeccionRepository.java
@@ -3,23 +3,24 @@ package utn.repositories;
 
 import org.springframework.stereotype.Repository;
 import utn.models.Coleccion;
+import utn.models.ColeccionDTO;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class ColeccionRepository {
-    private final List<Coleccion> colecciones = new ArrayList<>();
+    private final List<ColeccionDTO> colecciones = new ArrayList<>();
 
-    public List<Coleccion> getAll() {
+    public List<ColeccionDTO> getAll() {
         return colecciones;
     }
 
-    public void add(Coleccion coleccion) {
+    public void add(ColeccionDTO coleccion) {
         colecciones.add(coleccion);
     }
 
-    public void update(Coleccion coleccion) {
-        // Lógica para actualizar una colección, puede ser por nombre o id
+    public void update(ColeccionDTO coleccion) {
+        //TODO
     }
 }
