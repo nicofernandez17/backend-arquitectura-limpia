d1f1f1ef84ac201b0722adc5cb6d21573ea63b3f
SantyPrrr <santi.agus.perez.2002@gmail.com>
2025-07-01 12:27:10 -0300

    agrego lista ade apis externas a proxy service

--- components/fuente-proxy/src/main/java/utn/services/ProxyService.java
+++ components/fuente-proxy/src/main/java/utn/services/ProxyService.java
@@ -2,12 +2,15 @@ package utn.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import utn.model.HechoDTO;
 import utn.repositories.IHechoRepository;
 import utn.services.clientServices.DDSService;
+import utn.services.clientServices.IFuenteService;
 import utn.services.clientServices.MetaMapaService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,13 +20,15 @@ public class ProxyService {
     private final DDSService ddsService;
     private final MetaMapaService metaMapaService;
     private final IHechoRepository hechosRepository;
+    private final List<IFuenteService> apisExternas;
 
     public ProxyService(DDSService ddsService,
-                              MetaMapaService metaMapaService,
-                              IHechoRepository hechosRepository) {
+                        MetaMapaService metaMapaService,
+                        IHechoRepository hechosRepository) {
         this.ddsService = ddsService;
         this.metaMapaService = metaMapaService;
         this.hechosRepository = hechosRepository;
+        this.apisExternas = new ArrayList<>();
     }
 
     public Mono<List<HechoDTO>> cargarYObtenerHechos() {
@@ -31,5 +36,13 @@ public class ProxyService {
                 ddsService.getHechos()
                 //metaMapaService.getHechos()
         ).then(Mono.fromCallable(() -> hechosRepository.findAll()));
+    }
+
+    /* Lo mismo que la de arriba pero agarra los hechos de la lista de apis externas,
+     que debería tener las otras fuentes dentro */
+    public Mono<List<HechoDTO>> cargarYObtenerHechosV2() {
+        return Flux.fromIterable(apisExternas)
+                .flatMap(IFuenteService::getHechos) // ejecuta getHechos() para cada fuente
+                .then(Mono.fromCallable(() -> hechosRepository.findAll()));
     }
 }
