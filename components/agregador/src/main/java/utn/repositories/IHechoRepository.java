42b1cfaf24ef0db22de25c4fd618f8f7bcdcf7d3
SantyPerezzz <santi.agus.perez.2002@gmail.com>
2025-08-29 12:41:27 -0300

    Agrego Interfaces Repositories en Agregador

---
+++ components/agregador/src/main/java/utn/repositories/IHechoRepository.java
@@ -0,0 +1,7 @@
+package utn.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import utn.models.domain.Hecho;
+
+public interface IHechoRepository extends JpaRepository<Hecho, Long> {
+}
---
+++ components/agregador/src/main/java/utn/repositories/ISolicitudRepository.java
@@ -0,0 +1,7 @@
+package utn.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import utn.models.domain.SolicitudEliminacion;
+
+public interface ISolicitudRepository extends JpaRepository<SolicitudEliminacion, Long> {
+}
