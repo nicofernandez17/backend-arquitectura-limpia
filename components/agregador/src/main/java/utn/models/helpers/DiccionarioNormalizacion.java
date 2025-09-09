22ea71880b87ebec57508af401f24eb411871739
SantyPerezzz <santi.agus.perez.2002@gmail.com>
2025-09-09 11:23:16 -0300

    Base normalizador en Agregador

---
+++ components/agregador/src/main/java/utn/models/helpers/DiccionarioNormalizacion.java
@@ -0,0 +1,16 @@
+package utn.models.helpers;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+public class DiccionarioNormalizacion {
+
+    private Map<String, String> categorias;
+    private Map<String, String> provincias;
+
+
+}
--- components/agregador/src/main/java/utn/models/helpers/NormalizadorHechos.java
+++ components/agregador/src/main/java/utn/models/helpers/NormalizadorHechos.java
@@ -1,10 +1,29 @@
 package utn.models.helpers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import utn.models.domain.Hecho;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
 
 public class NormalizadorHechos {
 
+    private final Map<String, String> categorias;
+    private final Map<String, String> provincias;
+
+
     public NormalizadorHechos() {
+        ObjectMapper mapper = new ObjectMapper();
+        try(InputStream is = getClass().getClassLoader().getResourceAsStream("diccionario.normalizacion.json")){
+
+            DiccionarioNormalizacion dic = mapper.readValue(is, DiccionarioNormalizacion.class);
+
+        this.categorias = dic.getCategorias();
+        this.provincias = dic.getProvincias();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -17,7 +36,7 @@ public class NormalizadorHechos {
     }
 
     private void normalizarCategoria(Hecho hecho) {
-        //aca normalizamos
+
     }
 
     private void normalizarProvincia(Hecho hecho) {
