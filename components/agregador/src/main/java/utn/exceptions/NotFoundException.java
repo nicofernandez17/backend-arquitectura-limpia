bb09351c66e029c38f798c191f22c650288c3ba8
Nicolas Fernandez <nfernndez@frba.utn.edu.ar>
2025-10-14 16:41:05 -0300

    Login

---
+++ components/agregador/src/main/java/utn/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package utn.exceptions;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String entidad, String id) {
+        super("No se ha encontrado " + entidad + " de id " + id);
+    }
+}
---
+++ components/agregador/src/main/java/utn/models/dtos/TokenResponse.java
@@ -0,0 +1,15 @@
+package utn.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+    private String accessToken;
+    private String refreshToken;
+}
