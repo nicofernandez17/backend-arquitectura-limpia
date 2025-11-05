97661ea03f153b9c5d5af3676c4cc7bbe04c633a
SantyPerezzz <santi.agus.perez.2002@gmail.com>
2025-11-05 17:41:29 -0300

    SolicitudMapper y endpoint GET de solicitudes de eliminacion

--- components/agregador/src/main/java/utn/controllers/admin/SolicitudAdminController.java
+++ components/agregador/src/main/java/utn/controllers/admin/SolicitudAdminController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import utn.models.domain.SolicitudEliminacion;
 import utn.models.dtos.SolicitudDTO;
+import utn.models.dtos.SolicitudMapper;
 import utn.services.SolicitudService;
 
 import java.util.List;
@@ -21,9 +22,9 @@ public class SolicitudAdminController {
     }
 
     @GetMapping
-    public ResponseEntity<List<SolicitudEliminacion>> obtenerTodas(){
-        List<SolicitudDTO> solicitudes = solicitudService.obtenerSolicitudes().
-        return ResponseEntity.;
+    public ResponseEntity<List<SolicitudDTO>> obtenerTodas(){
+        List<SolicitudDTO> solicitudes = solicitudService.obtenerSolicitudes().stream().map(SolicitudMapper::toDTO).toList();
+        return ResponseEntity.ok(solicitudes);
     }
 
     @PostMapping("/{id}")
--- components/agregador/src/main/java/utn/models/dtos/SolicitudMapper.java
+++ components/agregador/src/main/java/utn/models/dtos/SolicitudMapper.java
@@ -1,4 +1,17 @@
 package utn.models.dtos;
 
+import utn.models.domain.SolicitudEliminacion;
+
 public class SolicitudMapper {
+
+    public static SolicitudDTO toDTO(SolicitudEliminacion solicitud) {
+        if (solicitud == null) return null;
+
+        return SolicitudDTO.builder()
+                .fechaCreacion(solicitud.getFechaCreacion())
+                .estado(solicitud.getEstado().name()) // Enum → String
+                .hecho(solicitud.getHecho() != null ? solicitud.getHecho().getTitulo() : "Sin título")
+                .motivo(solicitud.getMotivo())
+                .build();
+    }
 }
