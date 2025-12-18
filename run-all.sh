8c4144720eebf52ccf150e63d0dead9dfd4da4fc
Nicolas Fernandez <nfernndez@frba.utn.edu.ar>
2025-12-17 22:18:56 -0300

    fix script

--- run-all.sh
+++ run-all.sh
@@ -12,10 +12,10 @@ mvn clean install \
 
 echo "Levantando TODOS los servicios Spring Boot (sin logs)..."
 
-nohup java -jar agregador/target/agregador/agregador-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
-nohup java -jar fuente-dinamica/target/agregador/fuente-dinamica-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
-nohup java -jar fuente-proxy/target/fuente-proxy-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
-nohup java -jar fuente-estatica/target/fuente-estatica-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
-nohup java -jar utils/target/utils-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
+nohup java -jar components/agregador/target/agregador/agregador-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
+nohup java -jar components/fuente-dinamica/target/agregador/fuente-dinamica-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
+nohup java -jar components/fuente-proxy/target/fuente-proxy-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
+nohup java -jar components/fuente-estatica/target/fuente-estatica-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
+nohup java -jar components/utils/target/utils-1.0-SNAPSHOT.jar > /dev/null 2>&1 &
 
 echo "Todos los servicios levantados en segundo plano (sin logs)"
