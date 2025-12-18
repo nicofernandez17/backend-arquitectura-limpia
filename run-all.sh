fc3f48bb9eb845e4fdd58ab485a103a34a52033e
Nicolas Fernandez <nfernndez@frba.utn.edu.ar>
2025-12-17 21:57:00 -0300

    Fix script

--- run-all.sh
+++ run-all.sh
@@ -1,20 +1,23 @@
 set -e
 
-echo "Compilando todos los módulos (skip tests)..."
+echo "Compilando todos los módulos (skip tests + cobertura)..."
+
 mvn clean install \
-  -DskipTests \
+  -DskipTests=true \
   -Dmaven.test.skip=true \
   -Djacoco.skip=true \
   -Dcheckstyle.skip=true \
   -Dspotbugs.skip=true \
   -Dpmd.skip=true
 
-echo "Levantando servicios Spring Boot en AWS..."
+mkdir -p logs
 
-nohup java -jar agregador/target/agregador.jar > agregador.log 2>&1 &
-nohup java -jar fuente-dinamica/target/fuente-dinamica.jar > fuente-dinamica.log 2>&1 &
-nohup java -jar fuente-proxy/target/fuente-proxy.jar > fuente-proxy.log 2>&1 &
-nohup java -jar fuente-estatica/target/fuente-estatica.jar > fuente-estatica.log 2>&1 &
-nohup java -jar utils/target/utils.jar > utils.log 2>&1 &
+echo "Levantando TODOS los servicios Spring Boot..."
+
+nohup java -jar agregador/target/agregador/agregador-1.0-SNAPSHOT.jar > logs/agregador.log 2>&1 &
+nohup java -jar fuente-dinamica/target/agregador/fuente-dinamica-1.0-SNAPSHOT.jar > logs/fuente-dinamica.log 2>&1 &
+nohup java -jar fuente-proxy/target/fuente-proxy-1.0-SNAPSHOT.jar > logs/fuente-proxy.log 2>&1 &
+nohup java -jar fuente-estatica/target/fuente-estatica-1.0-SNAPSHOT.jar > logs/fuente-estatica.log 2>&1 &
+nohup java -jar utils/target/utils-1.0-SNAPSHOT.jar > logs/utils.log 2>&1 &
 
 echo "Todos los servicios levantados en segundo plano"
